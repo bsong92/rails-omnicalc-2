456dd84afe87c2d9a35ec962e6fde7b326404d41
Brian Song <brian.cs.song@gmail.com>
2024-10-30 21:11:42 +0000

    subtraction

--- app/controllers/subtraction_controller.rb
+++ app/controllers/subtraction_controller.rb
@@ -1,13 +1,13 @@
-class subtraction_controller < ApplicationController
-  def show_subtraction_form
+class SubtractionController < ApplicationController
+  def show_sub_form
     render({ :template => "subtraction_templates/subtraction_form" })
   end
 
-  def subtract_these
-    @first_number = params.fetch("first_num").to_f
-    @second_number = params.fetch("second_num").to_f
+  def subtract
+    @first_number = params.fetch("first_number").to_f
+    @second_number = params.fetch("second_number").to_f
 
-    @result =  @second_number - @first_number
+    @result =  @first_number - @second_number
 
     render({ :template => "subtraction_templates/subtract_results" })
   end
